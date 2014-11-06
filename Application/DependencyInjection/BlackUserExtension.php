21dbcead89d75c28dd5e9ad0f374c0815a18bc54
Alexandre 'pocky' Balmes <alexandre@lablackroom.com>
2014-11-06 11:23:43 -0500

    Add Mapping (invalid)

---
+++ Application/DependencyInjection/BlackUserExtension.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * This file is part of the Black package.
+ *
+ * (c) Alexandre Balmes <alexandre@lablackroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Black\Bundle\UserBundle\Application\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * Class BlackUserExtension
+ *
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ *
+ * @author  Alexandre 'pocky' Balmes <alexandre@lablackroom.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT
+ */
+class BlackUserExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $processor     = new Processor();
+        $configuration = new Configuration($this->getAlias());
+        $config        = $processor->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../Resources/config'));
+
+        if (!isset($config['db_driver'])) {
+            throw new \InvalidArgumentException('You must provide the black_user.db_driver configuration');
+        }
+
+        try {
+            $loader->load(sprintf('%s.xml', $config['db_driver']));
+        } catch (\InvalidArgumentException $e) {
+            throw new \InvalidArgumentException(
+                sprintf('The db_driver "%s" is not supported by engine', $config['db_driver'])
+            );
+        }
+
+        $container->setParameter($this->getAlias() . '.backend_type_' . $config['db_driver'], true);
+
+        $this->remapParametersNamespaces($config, $container, [
+            '' => [
+                'user_class' => 'black_user.user.model.class',
+                'user_manager' => 'black_user.user.manager.class',
+            ]
+        ]);
+
+        foreach ([] as $basename) {
+            $loader->load(sprintf('%s.xml', $basename));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlias()
+    {
+        return 'black_user';
+    }
+
+    /**
+     * @param array            $config
+     * @param ContainerBuilder $container
+     * @param array            $map
+     */
+    protected function remapParameters(array $config, ContainerBuilder $container, array $map)
+    {
+        foreach ($map as $name => $paramName) {
+            if (array_key_exists($name, $config)) {
+                $container->setParameter($paramName, $config[$name]);
+            }
+        }
+    }
+
+    /**
+     * @param array            $config
+     * @param ContainerBuilder $container
+     * @param array            $namespaces
+     */
+    protected function remapParametersNamespaces(array $config, ContainerBuilder $container, array $namespaces)
+    {
+        foreach ($namespaces as $ns => $map) {
+            if ($ns) {
+                if (!array_key_exists($ns, $config)) {
+                    continue;
+                }
+                $namespaceConfig = $config[$ns];
+            } else {
+                $namespaceConfig = $config;
+            }
+            if (is_array($map)) {
+                $this->remapParameters($namespaceConfig, $container, $map);
+            } else {
+                foreach ($namespaceConfig as $name => $value) {
+                    $container->setParameter(sprintf($map, $name), $value);
+                }
+            }
+        }
+    }
+}
---
+++ BlackUserBundle.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Black\Bundle\UserBundle;
+
+use Black\Bundle\UserBundle\Application\DependencyInjection\BlackUserExtension;
+use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Console\Application;
+
+class BlackUserBundle extends Bundle
+{
+    public function getContainerExtension()
+    {
+        return new BlackUserExtension();
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $mappings = array(
+            realpath($this->getPath() . '/Resources/config/doctrine/model') => 'Black\Component\User\Domain\Model',
+        );
+
+        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+
+        if (class_exists($ormCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    [],
+                    'application.backend_type_orm'
+                ));
+        }
+
+        $mongoCompilerClass = 'Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass';
+
+        if (class_exists($mongoCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineMongoDBMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    [],
+                    'application.backend_type_mongodb'
+                ));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerCommands(Application $application)
+    {
+        if (!is_dir($dir = $this->getPath().'/Command')) {
+            return;
+        }
+
+        $finder = new Finder();
+        $finder->files()->name('*Command.php')->in($dir);
+
+        $prefix = $this->getNamespace().'\\Command';
+        foreach ($finder as $file) {
+            $ns = $prefix;
+
+            if ($relativePath = $file->getRelativePath()) {
+                $ns .= '\\'.strtr($relativePath, '/', '\\');
+            }
+
+            $class = $ns.'\\'.$file->getBasename('.php');
+
+            if ($this->container) {
+                $alias = 'console.command.'.strtolower(str_replace('\\', '_', $class));
+
+                if ($this->container->has($alias)) {
+                    continue;
+                }
+            }
+
+            $r = new \ReflectionClass($class);
+
+            if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') &&
+                !$r->isAbstract() && !$r->getConstructor()->getNumberOfRequiredParameters()) {
+                    $application->add($r->newInstance());
+                }
+        }
+    }
+}
